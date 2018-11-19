35cbc36013e4504b7e06f29a4878b295fbcbbe6c
Quang Duy Thai <quang.td95@gmail.com>
2018-11-20 01:03:23 +0700

    first

---
+++ SVNTool/Form1.Designer.vb
@@ -0,0 +1,180 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.txtSvnPath = New System.Windows.Forms.TextBox()
+        Me.btnChooseFolder = New System.Windows.Forms.Button()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.txtSearch = New System.Windows.Forms.TextBox()
+        Me.btnSearch = New System.Windows.Forms.Button()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(13, 13)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(103, 17)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Path svn folder"
+        '
+        'txtSvnPath
+        '
+        Me.txtSvnPath.Location = New System.Drawing.Point(123, 13)
+        Me.txtSvnPath.Name = "txtSvnPath"
+        Me.txtSvnPath.Size = New System.Drawing.Size(445, 22)
+        Me.txtSvnPath.TabIndex = 1
+        '
+        'btnChooseFolder
+        '
+        Me.btnChooseFolder.Location = New System.Drawing.Point(575, 12)
+        Me.btnChooseFolder.Name = "btnChooseFolder"
+        Me.btnChooseFolder.Size = New System.Drawing.Size(75, 23)
+        Me.btnChooseFolder.TabIndex = 2
+        Me.btnChooseFolder.Text = "Choose Folder"
+        Me.btnChooseFolder.UseVisualStyleBackColor = True
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(82, 159)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(223, 23)
+        Me.Button1.TabIndex = 3
+        Me.Button1.Text = "Click here to download SVN CLI"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.BackColor = System.Drawing.Color.Transparent
+        Me.Label2.ForeColor = System.Drawing.Color.Red
+        Me.Label2.Location = New System.Drawing.Point(6, 162)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(70, 17)
+        Me.Label2.TabIndex = 4
+        Me.Label2.Text = "* required"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(43, 93)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(73, 17)
+        Me.Label3.TabIndex = 5
+        Me.Label3.Text = "Username"
+        '
+        'txtSearch
+        '
+        Me.txtSearch.Location = New System.Drawing.Point(123, 90)
+        Me.txtSearch.Name = "txtSearch"
+        Me.txtSearch.Size = New System.Drawing.Size(445, 22)
+        Me.txtSearch.TabIndex = 6
+        '
+        'btnSearch
+        '
+        Me.btnSearch.Location = New System.Drawing.Point(575, 90)
+        Me.btnSearch.Name = "btnSearch"
+        Me.btnSearch.Size = New System.Drawing.Size(75, 23)
+        Me.btnSearch.TabIndex = 7
+        Me.btnSearch.Text = "Search"
+        Me.btnSearch.UseVisualStyleBackColor = True
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(502, 171)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(161, 17)
+        Me.Label4.TabIndex = 8
+        Me.Label4.Text = "quangtd@minori.com.vn"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(137, 38)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(223, 17)
+        Me.Label5.TabIndex = 9
+        Me.Label5.Text = "Example: C:\SVN\SOURCE_CODE"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(137, 115)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(0, 17)
+        Me.Label6.TabIndex = 9
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Location = New System.Drawing.Point(137, 117)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(191, 17)
+        Me.Label7.TabIndex = 9
+        Me.Label7.Text = "Example: bp_minori_quangtd"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(675, 197)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.Label7)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.btnSearch)
+        Me.Controls.Add(Me.txtSearch)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.btnChooseFolder)
+        Me.Controls.Add(Me.txtSvnPath)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
+        Me.Text = "Q Perfect Tool for SVN"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents txtSvnPath As TextBox
+    Friend WithEvents btnChooseFolder As Button
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents txtSearch As TextBox
+    Friend WithEvents btnSearch As Button
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label6 As Label
+    Friend WithEvents Label7 As Label
+End Class
---
+++ SVNTool/Form1.vb
@@ -0,0 +1,149 @@
+﻿Imports System.IO
+Imports System.Text
+
+Public Class Form1
+    Private Sub btnChooseFolder_Click(sender As Object, e As EventArgs) Handles btnChooseFolder.Click
+        Dim FolderBrowserDialog1 = New FolderBrowserDialog()
+        If (FolderBrowserDialog1.ShowDialog() = DialogResult.OK) Then
+            txtSvnPath.Text = FolderBrowserDialog1.SelectedPath
+        End If
+    End Sub
+
+    Private Sub btnSearch_Click(sender As Object, e As EventArgs) Handles btnSearch.Click
+        Dim svnPath = txtSvnPath.Text.Trim
+        Dim query = txtSearch.Text.Trim
+        Dim logFilePath = Directory.GetCurrentDirectory & "\log.txt"
+        If String.IsNullOrWhiteSpace(svnPath) Then
+            MessageBox.Show("please input svn directory!", "pleeez")
+            Exit Sub
+        End If
+        If Not Directory.Exists(svnPath) Then
+            MessageBox.Show("Folder doesn't exists!", "pleeez")
+            Exit Sub
+        End If
+        If String.IsNullOrWhiteSpace(query) Then
+            MessageBox.Show("Please Write down something to search!", "pleeez")
+            Exit Sub
+        End If
+
+        Dim CommandSVN As String = String.Empty
+        CommandSVN &= "echo ""Please wait..."""
+        CommandSVN &= " & cd """ & svnPath & """"
+        CommandSVN &= " & " & svnPath.Chars(0) & ":"
+        CommandSVN &= " & svn log --search """ & query & """"
+        CommandSVN &= " -v > """ & logFilePath & """"
+
+        RunCommandCom(CommandSVN, String.Empty, False)
+
+        Dim lines = IO.File.ReadAllLines(logFilePath, System.Text.Encoding.GetEncoding("shift-jis"))
+
+        Dim fileList As New HashSet(Of String)
+        Dim fileAdded As New HashSet(Of String)
+        Dim fileDeleteWarning As New HashSet(Of String)
+
+        For i As Integer = lines.Count - 1 To 0 Step -1
+            Dim s As String = lines(i)
+            'neu file duoc add
+            If s.Contains("   A ") Then
+                'get path file
+                Dim path = s.Replace("   A ", "")
+                ' them vao danh sach add
+                If Not fileAdded.Contains(path) Then
+                    fileAdded.Add(path)
+                End If
+                'them vao danh sach modify
+                If Not fileList.Contains(path) Then
+                    fileList.Add(path)
+                End If
+                'neu trc do lo tay xoa, bi warning thi` xoa warning di
+                If fileDeleteWarning.Contains(path) Then
+                    fileDeleteWarning.Remove(path)
+                End If
+                ' neu file dc edit
+            ElseIf s.Contains("   M ") Then
+                Dim path = s.Replace("   M ", "")
+                'them vao danh sach modify
+                If Not fileList.Contains(path) Then
+                    fileList.Add(path)
+                End If
+                'neu file bi xoa
+            ElseIf s.Contains("   D ") Then
+                Dim path = s.Replace("   D ", "")
+                'xoa khoi danh sach file
+                If fileList.Contains(path) Then
+                    fileList.Remove(path)
+                End If
+                'neu file nay khong phai do minh add thi danh dau warning
+                If Not fileAdded.Contains(path) Then
+                    If Not fileDeleteWarning.Contains(path) Then
+                        fileDeleteWarning.Add(path)
+                    End If
+                Else
+                    'neu la file cua minh thi xoa no trong danh sach add.
+                    fileAdded.Remove(path)
+                End If
+            End If
+        Next i
+        If fileList.Count = 0 Then
+            MessageBox.Show("There is no data to collect", "plezz")
+            Exit Sub
+        Else
+            Dim outData As New StringBuilder
+
+            'outData.AppendLine("These files was added by " & query)
+            'outData.AppendLine("->")
+            'For Each dt In fileAdded
+            '    outData.AppendLine(dt)
+            'Next
+            'outData.AppendLine("=============================================================================================")
+            'outData.AppendLine("")
+
+            outData.AppendLine("These files were added/modified by " & query)
+            outData.AppendLine("->")
+            For Each dt In fileList
+                outData.AppendLine(dt)
+            Next
+            outData.AppendLine("=============================================================================================")
+            outData.AppendLine("")
+
+            outData.AppendLine("These files are not of " & query & " but he/she deleted")
+            outData.AppendLine("->")
+            For Each dt In fileDeleteWarning
+                outData.AppendLine(dt)
+            Next
+            outData.AppendLine("=============================================================================================")
+            outData.AppendLine("")
+
+            Dim bytesData() As Byte
+            Dim outFile = Directory.GetCurrentDirectory & "\output" & CLng(DateTime.UtcNow.Subtract(New DateTime(1970, 1, 1)).TotalMilliseconds) & ".txt"
+
+            bytesData = System.Text.Encoding.GetEncoding(932).GetBytes(outData.ToString)
+            Dim fs As New System.IO.FileStream(outFile,
+                                              System.IO.FileMode.Create,
+                                              System.IO.FileAccess.Write)
+            fs.Write(bytesData, 0, bytesData.Length)
+            fs.Close()
+            MessageBox.Show("Output of processing is stored at " & outFile)
+            My.Computer.FileSystem.DeleteFile(logFilePath)
+            Process.Start(Directory.GetCurrentDirectory)
+            Process.Start(outFile)
+        End If
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        MessageBox.Show("Please restart your computer after installing svn")
+        Dim webAddress As String = "https://sourceforge.net/projects/win32svn/files/latest/download"
+        Process.Start(webAddress)
+    End Sub
+
+    Sub RunCommandCom(command As String, arguments As String, permanent As Boolean)
+        Dim p As Process = New Process()
+        Dim pi As ProcessStartInfo = New ProcessStartInfo()
+        pi.Arguments = " " + If(permanent = True, "/K", "/C") + " " + command + " " + arguments
+        pi.FileName = "cmd.exe"
+        p.StartInfo = pi
+        p.Start()
+        p.WaitForExit()
+    End Sub
+End Class
